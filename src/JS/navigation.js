5e79d2304b1edb41682bccfdcdf71aadde7cac08
XRicko <idims52@gmail.com>
2020-07-15 00:01:56 +0300

    fixed mobile navigation

--- src/JS/navigation.js
+++ src/JS/navigation.js
@@ -1,6 +1,13 @@
 const hamburger = document.querySelector('.nav__hamburger');
 const navigation = document.querySelector('.navigation');
+const nav_link = document.querySelectorAll('.navigation__list-link');
 
 hamburger.addEventListener('click', () => {
   navigation.classList.toggle('open');
 });
+
+nav_link.forEach((nav_link) => {
+  nav_link.addEventListener('click', () => {
+    navigation.classList.toggle('open');
+  });
+}); 
